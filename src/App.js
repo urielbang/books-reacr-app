78c32e987b9d42a70e63cce7e9173f988b24a16c
urielbang <uri32655@gmail.com>
2023-11-28 10:43:40 +0200

    books-react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  function createBook(title) {}
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const editBookById = async (id, newTitle) => {
+    await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+    const upadetedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(upadetedBooks);
+  };
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    const updatedBooks = books.filter((book) => {
+      return id !== book.id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: title,
+    });
+
+    const updatedBooks = [...books, response.data];
+    setBooks(updatedBooks);
+  };
 
   return (
-    <div>
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,21 @@
-function BookList() {
-  return <div>BookList</div>;
+import { useContext } from "react";
+import BookShow from "./BookShow";
+import BooksContext from "../context/books";
+function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
+  const renderdBooks = books.map((book) => {
+    return (
+      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+    );
+  });
+  return (
+    <div className="book-list">
+      {count}
+      <button className="button" onClick={incrementCount}>
+        Click!
+      </button>
+      {renderdBooks}
+    </div>
+  );
 }
 export default BookList;
