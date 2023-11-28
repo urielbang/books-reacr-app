955b4d75c31e2f8b71cf60c7666b03f94ba3f89e
urielbang <uri32655@gmail.com>
2023-11-28 11:55:11 +0200

    little-hooks

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,9 @@
-import { useContext } from "react";
 import BookShow from "./BookShow";
-import BooksContext from "../context/books";
+
+import useBooksContext from "../hooks/use-books-context";
+
 function BookList() {
-  const { books } = useContext(BooksContext);
+  const { books } = useBooksContext();
 
   const renderdBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} />;
